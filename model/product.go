68513172761773896b21ba301a2e368cd672380a
Arlind <arlind12344@gmail.com>
2023-11-27 11:44:15 +0100

    Refactor Product struct in model package

--- model/product.go
+++ model/product.go
@@ -1,12 +1,14 @@
 package model
 
 // Product struct
-type Product struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Category    string `json:"category"`
-	Amount      int    `json:"amount"`
-}
+type (
+	Product struct {
+		Name        string `json:"name" validate:"required min=2 max=32"`
+		Description string `json:"description"`
+		Category    string `json:"category"`
+		Amount      int    `json:"amount"`
+	}
+)
 
 // Products struct
 type Products struct {
