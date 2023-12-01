22b1bb9d24b326ff3448efa953c88d1cda543980
Arlind <arlind12344@gmail.com>
2023-12-01 14:25:39 +0100

    Fix authentication middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -15,32 +15,32 @@ import (
 // @Tags middleware
 // @Accept json
 // @Produce json
-// @Router /auth [get]
+// @Router /auth [post]
 func Auth() func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		username := config.Config("AUTH_USERNAME")
 		password := config.Config("AUTH_PASSWORD")
-		log.Print(">> Inside AuthReq middleware")
+		log.Print(">> Inside Auth middleware")
 
 		// Check if the username and password are correct
 		if c.FormValue("username") == username && c.FormValue("password") == password {
 			// Create a new token object, specifying signing method and the claims
 			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 				"username": username,
+				"iss":      "your_issuer", // Set the issuer
 				"exp":      time.Now().Add(time.Hour * 72).Unix(),
 			})
 
 			// Sign and get the complete encoded token as a string using the secret
-			tokenString, err := token.SignedString([]byte("your_secret"))
+			tokenString, err := token.SignedString([]byte(config.Config("JWT_SECRET")))
 			if err != nil {
 				return c.SendStatus(fiber.StatusInternalServerError)
 			}
-			log.Print("<< AuthReq middleware tokenString: ", tokenString)
+			log.Print("<< Auth middleware tokenString: ", tokenString)
 			return c.JSON(fiber.Map{"token": tokenString})
 		}
-		log.Print(">> AuthReq middleware: username or password is incorrect")
-		log.Printf(">> \nYour input username is: %s\nYour input password is %s", c.FormValue("username"), c.FormValue("password"))
-		log.Printf(">> \nThe correct username is: %s\nThe correct password is %s", username, password)
+
+		log.Print(">> Auth middleware: username or password is incorrect")
 		return c.SendStatus(fiber.StatusUnauthorized)
 	}
 }
@@ -53,14 +53,44 @@ func Auth() func(*fiber.Ctx) error {
 // @Produce json
 // @Router /api [get]
 func AuthReq() func(*fiber.Ctx) error {
-	// check if the token is valid
 	return func(c *fiber.Ctx) error {
 		log.Print(">> Inside AuthReq middleware")
-		token := c.Get("Authorization")
-		if token == "" {
+		tokenString := c.Get("Authorization")
+		if tokenString == "" {
+			// No token, return error
 			return c.SendStatus(fiber.StatusUnauthorized)
 		}
-		log.Print("<< AuthReq middleware: token: ", token)
+
+		// Remove Bearer from token
+		// Parse the token
+		tokenString = tokenString[7:]
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Make sure that the token method conforms to "SigningMethodHMAC"
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fiber.ErrUnauthorized
+			}
+			return []byte(config.Config("JWT_SECRET")), nil
+		})
+		log.Printf("<< AuthReq middleware token: %+v\n", token)
+		log.Printf("<< AuthReq middleware err: %+v\n", err)
+
+		if err != nil {
+			// Handle specific errors, e.g., token expired, issuer mismatch, etc.
+			switch err.(type) {
+			case *jwt.ValidationError:
+				return c.SendStatus(fiber.StatusUnauthorized)
+			default:
+				return c.SendStatus(fiber.StatusInternalServerError)
+			}
+		}
+
+		log.Print("<< AuthReq middleware token.Valid: ", token.Valid)
+		if !token.Valid {
+			// Invalid token, return error
+			return c.SendStatus(fiber.StatusUnauthorized)
+		}
+
+		// Token is valid, proceed with the request
 		return c.Next()
 	}
 }
